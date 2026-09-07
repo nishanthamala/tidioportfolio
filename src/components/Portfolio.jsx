6aa0d1b10877bf2a9c5a4fb62895f00e6ba1aede
amal <nishanthamala7@gmail.com>
2026-09-07 07:19:33 -0700

    price,portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -21,9 +21,9 @@ import Reveal from './Reveal'
 */
 
 const portfolioVideos = [
-  { src: '/videos/construction.mp4', poster: '/images/congratulations img.png', title: 'Room Tour' },
-  { src: '/videos/Education centre.mp4', poster: '/images/educational centre img.png', title: 'Hotel Overview ' },
-  { src: '/videos/hotel-service.mp4', poster: '/images/hotel-service.jpeg', title: 'Hotel Services' },
+  { src: '/videos/construction.mp4', poster: '/images/congratulations img.png', title: 'ABC Constructions' },
+  { src: '/videos/Education centre.mp4', poster: '/images/educational centre img.png', title: 'ABC Education' },
+  { src: '/videos/hotel-service.mp4', poster: '/images/hotel-service.jpeg', title: 'ABC Hotel' },
   { src: '/videos/seaview-resort.mp4', poster: '/images/seaview-resort.jpeg', title: 'Seaview Resort' },
 
 ]

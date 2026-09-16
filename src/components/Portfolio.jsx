8d7a67ee165f721e314e7b8f181f0372a8c47933
nishanth <nishanthamala7@gmail.com>
2026-09-16 14:51:07 +0530

    portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -19,11 +19,18 @@ const Portfolio = () => {
           <div className="portfolio__item glass">
             <div className="portfolio__item-image">
               <img src="/images/om-muruga-construction.png" alt="Om Muruga Construction project by tavonandtech" />
-              <img src="/images/ecrinn.png" alt="ECR INN  project by tavonandtech" />
-
             </div>
             <div className="portfolio__item-meta">
               <span className="portfolio__item-title">Good work by tavonandtech -- by Om Muruga Construction</span>
+            </div>
+          </div>
+
+          <div className="portfolio__item glass">
+            <div className="portfolio__item-image">
+              <img src="/images/om-muruga-construction-2.png" alt="ECR INN project by tavonandtech" />
+            </div>
+            <div className="portfolio__item-meta">
+              <span className="portfolio__item-title">ECR INN</span>
             </div>
           </div>
         </div>
